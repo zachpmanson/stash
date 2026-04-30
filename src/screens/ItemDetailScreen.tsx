293d59bf415e9054caca610f028413884718b7eb
Zach Manson <zachpmanson@gmail.com>
2026-04-30 22:52:41 +1000

    Add border to html images and figures

--- src/screens/ItemDetailScreen.tsx
+++ src/screens/ItemDetailScreen.tsx
@@ -316,8 +316,8 @@ const htmlTagStyles = StyleSheet.create({
     borderRadius: Radius.sm,
     fontFamily: "monospace",
   },
-  img: { marginVertical: Spacing.sm },
-  figure: { marginVertical: Spacing.sm },
+  img: { marginVertical: Spacing.sm, borderLeftWidth: 3, borderLeftColor: Colors.border },
+  figure: { marginVertical: Spacing.sm, borderLeftWidth: 3, borderLeftColor: Colors.border },
   p: {
     marginVertical: Spacing.sm,
   },
